8c8eb9558d5b724a2c7df9ebd3fdd26f17e4aacd
JohanMusic05 <89704545+electroJohanHe05@users.noreply.github.com>
2021-11-25 15:57:08 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num1=0;
+int num2=0;
+int resultado=0;
+cout<<"Digite un número";
+cin>>num1;
+cout<<"Digite un número";
+cin>>num2;
+resultado=num1*num2;
+cout<<"El producto de los números"<<resultado;
+resultado=num1+num2;
+cout<<"La suma de los números"<<resultado;
+resultado=num1-num2;
+cout<<"La resta de los números"<<resultado;
+ 
+ 
+ 
+ 
+ 
+ 
+}
